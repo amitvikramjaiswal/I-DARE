4eaf71add899f98c48c131a33f9f65e3a72fb5e3
Amit Vikram Jaiswal <amitvikramjaiswal@gmail.com>
2016-05-27 12:51:52 +0530

    Utility, BaseFragment

---
+++ IDARE/app/src/main/java/com/opensource/app/idare/utils/Utility.java
@@ -0,0 +1,7 @@
+package com.opensource.app.idare.utils;
+
+/**
+ * Created by ajaiswal on 5/27/2016.
+ */
+public class Utility {
+}
---
+++ IDARE/app/src/main/java/com/opensource/app/idare/view/fragment/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.opensource.app.idare.view.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ajaiswal on 5/27/2016.
+ */
+public class BaseFragment extends Fragment {
+}
