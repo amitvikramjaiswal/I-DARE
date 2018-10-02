139d19c72f5c657d55f8dd571cbaa5267ed1fd05
amitvikramjaiswal <amitvikramjaiswal@gmail.com>
2018-10-02 23:24:19 +0530

    near by members on active mode map

--- IDARE/app/src/main/java/com/opensource/app/idare/view/activity/ActiveModeActivity.java
+++ IDARE/app/src/main/java/com/opensource/app/idare/view/activity/ActiveModeActivity.java
@@ -1,22 +1,32 @@
 package com.opensource.app.idare.view.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.maps.android.clustering.ClusterManager;
 import com.opensource.app.idare.R;
 import com.opensource.app.idare.databinding.ActivityActiveModeBinding;
+import com.opensource.app.idare.model.data.entity.NearBySafeHouseResultEntity;
 import com.opensource.app.idare.viewmodel.ActiveModeViewModel;
 import com.opensource.library.sosmodelib.view.activity.MapActivity;
 
-public class ActiveModeActivity extends MapActivity {
+import java.util.List;
+
+public class ActiveModeActivity extends MapActivity implements ActiveModeViewModel.DataListener {
 
     private static final String TAG = ActiveModeActivity.class.getSimpleName();
     private ActivityActiveModeBinding binding;
     private Toolbar toolbar;
     private ActiveModeViewModel viewModel;
+
+    public static Intent getStartIntent(Context context) {
+        return new Intent(context, ActiveModeActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +46,19 @@ public class ActiveModeActivity extends MapActivity {
         getSupportFragmentManager().beginTransaction().add(R.id.map_container, mapFragment).commit();
         mapFragment.getMapAsync(this);
 
-        viewModel = new ActiveModeViewModel(this);
+        viewModel = new ActiveModeViewModel(this, this);
+        binding.setViewModel(viewModel);
+    }
+
+    @Override
+    public void setUpClusterer() {
+        clusterManager = new ClusterManager<NearBySafeHouseResultEntity>(this, googleMap);
+        super.setUpClusterer();
+    }
+
+    @Override
+    public void addClusterItem(List clusterItems) {
+        super.addClusterItem(clusterItems);
+        setBounds(clusterItems);
     }
 }
--- IDARE/app/src/main/java/com/opensource/app/idare/viewmodel/ActiveModeViewModel.java
+++ IDARE/app/src/main/java/com/opensource/app/idare/viewmodel/ActiveModeViewModel.java
@@ -1,17 +1,47 @@
 package com.opensource.app.idare.viewmodel;
 
 import android.content.Context;
+import android.util.Log;
+
+import com.opensource.app.idare.model.data.entity.UserProfileResponseModel;
+import com.opensource.app.idare.model.service.handler.IDAREResponseHandler;
+import com.opensource.app.idare.model.service.impl.SessionFacadeImpl;
+import com.opensource.app.idare.utils.IDAREErrorWrapper;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ActiveModeViewModel extends IDareBaseViewModel {
 
     private static final String TAG = ActiveModeViewModel.class.getSimpleName();
+    private DataListener dataListener;
 
-    public ActiveModeViewModel(Context context) {
+    public ActiveModeViewModel(Context context, DataListener dataListener) {
         super(context);
+        this.dataListener = dataListener;
+        getNearByUsers();
+    }
+
+    private void getNearByUsers() {
+        SessionFacadeImpl.getInstance().fetchNearByUsers(context, new IDAREResponseHandler.ResponseListener<UserProfileResponseModel[]>() {
+            @Override
+            public void onSuccess(UserProfileResponseModel[] nearByUsers) {
+                Log.d(TAG, nearByUsers.toString());
+                dataListener.setUpClusterer();
+                dataListener.addClusterItem(Arrays.asList(nearByUsers));
+            }
+        }, new IDAREResponseHandler.ErrorListener() {
+            @Override
+            public void onError(IDAREErrorWrapper error) {
+                Log.e(TAG, error.getMessage());
+            }
+        });
     }
 
     public interface DataListener extends IDareBaseViewModel.DataListener {
+        void addClusterItem(List<UserProfileResponseModel> nearByUsers);
 
+        void setUpClusterer();
     }
 
 }
